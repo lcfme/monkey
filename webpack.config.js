ac1d058a6393a24b595de858c0ab391214aa9a04
lcfme <lcfme@sina.com>
2018-12-30 15:34:09 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const resolve = path.resolve.bind(path, __dirname);
+
+module.exports = env => {
+  return {
+    entry: resolve('lib/index'),
+    mode: env && env.production ? 'production' : 'development',
+    output: {
+      filename: 'monkey.js',
+      library: 'Monkey',
+      libraryTarget: 'umd',
+      path: resolve('out')
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          include: resolve('src')
+        }
+      ]
+    }
+  };
+};
